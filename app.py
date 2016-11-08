457d42d9a7ff776fdcb51c601e349e2b7555a3e2
yingchi <yingchi.pei@gmail.com>
2016-11-08 21:43:34 +0800

    read and save only the filename instead of the full path

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask
 from flask import request, render_template, jsonify
 
 
-
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	datafiles = []
+    datafiles = []
     for root, dirs, files in os.walk('./static/data'):
         for filename in [os.path.join(root, name) for name in files]:
             if not filename.endswith('.txt'):
                 continue
-            datafiles.append(filename)
+            datafiles.append(os.path.basename(filename))
+
     return render_template('index.html', **{
         'datafiles': datafiles
     })
